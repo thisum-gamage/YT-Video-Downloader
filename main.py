e3187ebff869a2c806756440bf8a7ea54a1e7db2
Thisum Gamage <thisumgmg@gmail.com>
2026-05-26 22:03:43 +0530

    Refactor download_video function to include save_path parameter

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-def download_video(url):
+def download_video(url, save_path):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
@@ -12,7 +12,3 @@ def download_video(url):
         print("Video Downloaded Successfully!!!")
     except Exception as e:
         print(e)
-
-
-url = ""
-save_path = ""
