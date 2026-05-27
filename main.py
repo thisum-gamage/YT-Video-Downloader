c5e4a9b3e5835fcc3e1c052479eadfa2bd0f82bf
Thisum Gamage <thisumgmg@gmail.com>
2026-05-27 20:28:51 +0530

    Remove commented-out code and streamline video download process

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+# from pytube import YouTube
+# import tkinter as tk
+# from tkinter import filedialog
+
+
+# def download_video(url, save_path):
+#     try:
+#         yt = YouTube(url)
+#         streams = yt.streams.filter(progressive=True, file_extension="mp4")
+#         highest_res_stream = streams.get_highest_resolution()
+#         highest_res_stream.download(output_path=save_path)
+#         print("Video Downloaded Successfully!!!")
+#     except Exception as e:
+#         print(e)
+
+
 from pytube import YouTube
-import tkinter as tk
-from tkinter import filedialog
 
+try:
+    url = input("Paste your video url : ")
+    yt = YouTube(url)
 
-def download_video(url, save_path):
-    try:
-        yt = YouTube(url)
-        streams = yt.streams.filter(progressive=True, file_extension="mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path=save_path)
-        print("Video Downloaded Successfully!!!")
-    except Exception as e:
-        print(e)
+    print(yt.title)
+
+    minutes, seconds = divmod(yt.length, 60)
+
+    print(f"Video Length: {minutes}:{seconds}")
+    print("Downloading...")
+
+    video = yt.streams.get_highest_resolution()
+    download = video.download()
+
+    print("Download Completed! 🎉")
+except:
+    print("Please enter valid url!!!")
