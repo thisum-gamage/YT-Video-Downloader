beb88c2a3758ddee71badca846ee40577a3b346a
Thisum Gamage <thisumgmg@gmail.com>
2026-05-29 12:45:59 +0530

    Remove unused imports and commented-out code to clean up main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# from pytube import YouTube
-# import tkinter as tk
-# from tkinter import filedialog
-
-
-# def download_video(url, save_path):
-#     try:
-#         yt = YouTube(url)
-#         streams = yt.streams.filter(progressive=True, file_extension="mp4")
-#         highest_res_stream = streams.get_highest_resolution()
-#         highest_res_stream.download(output_path=save_path)
-#         print("Video Downloaded Successfully!!!")
-#     except Exception as e:
-#         print(e)
-
 # https://youtu.be/9wafxM-vA0E
 
 import tkinter as tk
