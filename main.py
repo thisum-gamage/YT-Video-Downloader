34047c0cd166cbd3e91e56787c26e5756ec6643d
Thisum Gamage <thisumgmg@gmail.com>
2026-05-29 12:45:05 +0530

    Refactor download process to use yt-dlp and implement threading for improved performance

--- main.py
+++ main.py
@@ -15,30 +15,38 @@
 
 # https://youtu.be/9wafxM-vA0E
 
-from pytube import YouTube
 import tkinter as tk
+import yt_dlp
+import threading
+
+
+def start_download_thread():
+    thread = threading.Thread(target=download_video)
+    thread.start()
 
 
 def download_video():
     try:
-        url = url_input.get()
-
         title_label.config(text="")
         length_label.config(text="")
-        
-        yt = YouTube(url)
-        title_label.config(text=f"Title: {yt.title}")
+        ydl_opts = {}
+        url = url_input.get()
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
-        minutes, seconds = divmod(yt.length, 60)
-        length_label.config(text=f"Video Length: {minutes}:{seconds}")
+            info_dict = ydl.extract_info(url, download=False)
 
-        status_label.config(text="Downloading...", fg="blue")
-        root.update_idletasks()
+            video_title = info_dict.get("title", None)
+            video_duration = info_dict.get("duration", None)
 
-        video = yt.streams.get_highest_resolution()
-        video.download()
+            title_label.config(text=f"Title : {video_title}")
+            minutes, seconds = divmod(video_duration, 60)
+            length_label.config(text=f"Video Length : {minutes}:{seconds}")
 
-        status_label.config(text="Download Completed! 🎉", fg="green")
+            status_label.config(text="Downloading...", fg="blue")
+            root.update_idletasks()
+            ydl.download([url])
+
+            status_label.config(text="Download Completed!", fg="green")
     except Exception as e:
         print(e)
         status_label.config(text="Please enter valid url!!!", fg="red")
@@ -59,7 +67,7 @@ url_input.pack(pady=5)
 down_btn = tk.Button(
     root,
     text="Download",
-    command=download_video,
+    command=start_download_thread,
     bg="red",
     fg="white",
     font=("Arial", 10, "bold"),
@@ -70,9 +78,9 @@ status_label = tk.Label(root, text="", font=("Arial", 10, "bold"))
 status_label.pack(pady=5)
 
 title_label = tk.Label(root, text="", wraplength=400)
-title_label.pack(pady=10)
+title_label.pack(pady=2)
 
 length_label = tk.Label(root, text="")
-length_label.pack(pady=10)
+length_label.pack(pady=2)
 
 root.mainloop()
